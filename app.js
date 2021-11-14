3514cd10985002db96457a6cd5f7b05229dcc6c6
Chinguun <Chinguunv@gmail.com>
2021-11-14 19:28:00 +0800

    Togloomiin onoog 100 bolgoloo

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
       scores[activePlayer];
 
     // Ug toglogch hojson esehiig (onoog ni 100-s ih eseh) shalgah
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Togloomiig duussan tuluvt oruulna
       isNewGame = false;
 
